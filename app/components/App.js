5c2c88cedad5e59010c7cdf4c8d4ce1ceecee6f3
Tyler Henry <tyler@milmerry.com>
2019-03-01 22:19:26 -0500

    Adding React and Precompiler

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './Button/Button';
+
+ReactDOM.render(
+    <Button>Hello, world!</Button>,
+    document.getElementById('app')
+  );
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
 
 module.exports = {
 	mode: 'development',
 	entry: {
-		app: './app/components/index.js'
+        vendor: './app/vendor/js/index.js',
+        app: './app/components/App.js',
 	},
 	devServer: {
 		contentBase: './app/component/',
@@ -17,15 +19,30 @@ module.exports = {
 	},
 	devtool: 'inline-source-map',
 	output: {
-		filename: 'react.bundle.min.js',
-		path: path.resolve(__dirname, 'app/static/js/'),
+		filename: '[name].bundle.min.js',
+		path: path.resolve(__dirname, 'dist/'),
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
-			template: 'app/index.html'
-		}),
-	],
+            template: 'app/index.html',
+            chunksSortMode: function(a, b) {
+                return (a.names[0] < b.names[0])? 1 : -1;
+            }
+        }),
+        // new BundleAnalyzerPlugin()
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups:{
+                node_vendors:{
+                    test: '/[\\/]node_modules[\\/]/',
+                    chunks: 'async',
+                    priority: 1
+                }
+            }
+        }
+    },
 	module: {
 		rules: [
 			{
